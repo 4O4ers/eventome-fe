9ebcbed704e739b6abaf8e1c318de938fbe354cc
balqessAlfasatlah <BalqeesAlfasatlah@yahoo.com>
2021-08-20 20:50:42 +0300

    about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,13 +1,69 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Carousel } from 'react-bootstrap';
 
-class AboutUs extends Component {
+
+export class About extends Component {
     render() {
         return (
             <div>
-                <h1>about us</h1>
+                <h1 style={{ textAlign : "left", width : "20rem" , marginTop : "20px"  , padding : "20px" }} >Our Work</h1>
+                <p style={{ width : "20rem"}}>We created this site" Eventome " that provides services in everything related to events , </p>
+                <h2 style={{ textAlign : "center"}}>Meet Our Team</h2>
+
+                <Carousel style={{width :"30%" , margin :"auto"}}>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://cdn.iconscout.com/icon/free/png-512/user-1648810-1401302.png"
+                            alt="Hamza Ahmad"
+                        />
+                        <Carousel.Caption>
+                            <h3>Hamza Ahmad</h3>
+                            <p>Software Development</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://cdn.iconscout.com/icon/free/png-512/user-1648810-1401302.png"
+                            alt="Balqees Alfasatlah"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Balqees Alfasatlah</h3>
+                            <p>Software Development</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://cdn.iconscout.com/icon/free/png-512/user-1648810-1401302.png"
+                            alt="Ali Alhjouj"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Ali Alhjouj</h3>
+                            <p>Software Development</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="https://cdn.iconscout.com/icon/free/png-512/user-1648810-1401302.png"
+                            alt="Adham"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Adham</h3>
+                            <p>Software Development</p>
+                        </Carousel.Caption>
+                    </Carousel.Item> 
+                </Carousel>
             </div>
         )
     }
 }
 
-export default AboutUs
+export default About
