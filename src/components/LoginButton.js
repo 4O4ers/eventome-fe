27ec52a08f630508239c9bda9fe3ee13e34421e5
balqessAlfasatlah <BalqeesAlfasatlah@yahoo.com>
2021-08-21 14:55:50 +0300

    about us

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <p style={{color: 'white', cursor: 'pointer', textAlign: 'center', height: '100%', margin: 'auto 0', display: 'flex', alignItems: 'center'}} onClick={() => loginWithRedirect()}>Log In</p>;
+  return <p className="loginClass"  onClick={() => loginWithRedirect()}>Log In</p>;
 };
 
 export default LoginButton;
